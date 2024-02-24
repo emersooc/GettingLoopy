22849d6396eacb29330661f28217366949fc029b
emersooc <emersooc@mail.uc.edu>
2024-02-24 15:11:52 -0500

    Part B Complete/Start FtoC/CtoF

--- src/GettingLoopy.java
+++ src/GettingLoopy.java
@@ -4,24 +4,61 @@ public class GettingLoopy
     {
         int counter; //declaring counter variable
 
+        //A1
         for (counter = 1; counter <= 30; counter++)
         {
             System.out.println(counter); //prints during each iteration
         }
 
+        //A2
         for (counter = 30; counter >= 0; counter--)
         {
             System.out.println(counter); //prints during each iteration
         }
 
+        //A3
         for (counter = 0; counter <=18; counter = counter + 3)
         {
             System.out.println(counter); //prints during each iteration
         }
 
+        //A4
         for (counter = 10; counter >=0; counter = counter - 2)
         {
             System.out.println(counter); //prints during each iteration
+        }
+
+
+        int starCounter; //variable for producing star pattern
+
+        //B5
+        for (counter = 1; counter <= 5; counter++)
+        {
+            for (starCounter = 1; starCounter <= counter; starCounter++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(); //starts new line
+        }
+
+        //B6
+        for (counter = 1; counter <= 5; counter++)
+        {
+            for (starCounter = 5; starCounter >= counter; starCounter--)
+            {
+                System.out.print("*");
+            }
+            System.out.println(); //starts new line
+        }
+
+        //B7
+        for (counter = 1; counter <= 5; counter++)
+        {
+            for (starCounter = 5; starCounter >= 1; starCounter--)
+            {
+                System.out.print("*");
+            }
+            System.out.println(); //starts new line
         }
     }
 }
