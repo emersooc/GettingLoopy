b2a97785ee0bb3149326b6ed0c11967807c079b2
emersooc <emersooc@mail.uc.edu>
2024-02-24 14:54:46 -0500

    Part A Complete/Start Part B

--- src/GettingLoopy.java
+++ src/GettingLoopy.java
@@ -2,6 +2,26 @@ public class GettingLoopy
 {
     public static void main(String[] args)
     {
+        int counter; //declaring counter variable
 
+        for (counter = 1; counter <= 30; counter++)
+        {
+            System.out.println(counter); //prints during each iteration
+        }
+
+        for (counter = 30; counter >= 0; counter--)
+        {
+            System.out.println(counter); //prints during each iteration
+        }
+
+        for (counter = 0; counter <=18; counter = counter + 3)
+        {
+            System.out.println(counter); //prints during each iteration
+        }
+
+        for (counter = 10; counter >=0; counter = counter - 2)
+        {
+            System.out.println(counter); //prints during each iteration
+        }
     }
 }
